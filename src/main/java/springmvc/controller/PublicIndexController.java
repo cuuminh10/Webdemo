0e1fabdcb540b575416146ef14c83f368f75b88f
cuumi <cuumi@DESKTOP-4DP0JH8>
2019-03-06 16:54:57 +0700

    Complete Setting

--- src/main/java/springmvc/controller/PublicIndexController.java
+++ src/main/java/springmvc/controller/PublicIndexController.java
@@ -1,11 +1,14 @@
 package springmvc.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import springmvc.bean.Products;
 import springmvc.constant.Defines;
 import springmvc.dao.ProductsDAO;
 
@@ -24,7 +27,7 @@ public class PublicIndexController {
 	}
 	@RequestMapping("")
 	public String index(ModelMap map) {
-		
+		List<Products> Item = ProductsDAO.getItems();
 		return "public.index";
 	}
 	
